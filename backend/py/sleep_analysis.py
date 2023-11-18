b5d9906d7c8aca685cbc973b067cb4eefc942a8f
milanXpetrovic <milan.petrovic@uniri.hr>
2023-11-18 18:51:02 +0000

    feat: save files and zip

--- backend/py/sleep_analysis.py
+++ backend/py/sleep_analysis.py
@@ -3,13 +3,38 @@ Created on Mon Oct  7 23:19:07 2019
 @author: icecream boi
 """
 # %%
+import os 
+import zipfile
 
-import argparse
-import time 
+from datetime import datetime, timedelta
 from dateutil import parser
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def create_datetime_interval(start_date_time, end_date_time):
+    times_list = []
+    while start_date_time < end_date_time:
+        next_date = start_date_time + timedelta(hours=23, minutes=59)
+        # Handling the time interval strings in the required format
+        start_str = start_date_time.strftime('%Y-%m-%d %H:%M:%S')
+        end_str = next_date.strftime('%Y-%m-%d %H:%M:%S')
+        times_list.append([start_str, end_str])
+        start_date_time = next_date + timedelta(minutes=1)
+    
+    return times_list
+
+
+def zip_dir(directory, file_name):
+    zip_file = os.path.join(SAVE_DIR, f'{file_name}.zip')
+
+    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, directory))
 
 
 def get_mean_and_sem_by_hour(df):
@@ -39,30 +64,28 @@ def find_amount_of_sleep(df):
     return df_sleeping
 
 
-# TODO: read these dates from a file
-
-
-INPUT_FRONTEND = {
+INPUT_FRONT = {
     'file_path': 'dummy_df.csv',
     'file_name': '121123bobiCtM008',
     'start_date': '2023/11/13',
     'end_date': '2023/11/18'
 }
-# cols: 1-16 (CsCh)
-# time format : YYYY/MM/DD
-start_time = '08:00:00'
-end_time = '07:59:00'
 
-start_date_time = f'{INPUT_FRONTEND["start_date"]} {start_time}'
-end_date_time = f'{INPUT_FRONTEND["end_date"]} {end_time}'
+SAVE_DIR = './../output/'
 
-df = pd.read_csv(INPUT_FRONTEND['file_path'], sep='\t', header=None, index_col=0)
+start_date_time =  parser.parse( f'{INPUT_FRONT["start_date"]} 08:00:00')
+end_date_time =  parser.parse(f'{INPUT_FRONT["end_date"]} 07:59:00')
 
+df = pd.read_csv(INPUT_FRONT['file_path'], sep='\t', header=None, index_col=0)
 df['datetime'] = pd.to_datetime(df.iloc[:, 0] + ' ' + df.iloc[:, 1])
 df.set_index('datetime', inplace=True)
-
 df.drop(df.columns[:9], axis=1, inplace=True)
 df.columns = list(range(1, 33))
+
+times_list = create_datetime_interval(start_date_time, end_date_time)
+
+SCRIPT_OUTPUT = os.path.join(SAVE_DIR, INPUT_FRONT['file_name'])
+os.makedirs(SCRIPT_OUTPUT, exist_ok=True)
 
 for time_start_end in times_list:
     START, END = time_start_end
@@ -70,13 +93,14 @@ for time_start_end in times_list:
     start_time = parser.parse(START).strftime("%d_%m_%y_%H_%M")
     ent_time =  parser.parse(END).strftime("%d_%m_%y_%H_%M")
     
-    FILE_NAME = f"{file_name}_{start_time}_to_{ent_time}"
+    FILE_NAME = f"{start_time}_to_{ent_time}"
 
     df_mean, df_sem = get_mean_and_sem_by_hour(df_)
     df_sleep = find_amount_of_sleep(df_)
+   
+    df_mean.to_csv(os.path.join(SCRIPT_OUTPUT, f'{FILE_NAME}_mean.csv'))
+    df_sem.to_csv(os.path.join(SCRIPT_OUTPUT, f'{FILE_NAME}_sem.csv'))
+    df_sleep.to_csv(os.path.join(SCRIPT_OUTPUT, f'{FILE_NAME}_sleep.csv'))
 
-    res = {
-        f'{FILE_NAME}_mean': df_mean,
-        f'{FILE_NAME}_sem': df_sem,
-        f'{FILE_NAME}_sleep': df_sleep
-    }
+
+zip_dir(SCRIPT_OUTPUT, INPUT_FRONT['file_name'])
